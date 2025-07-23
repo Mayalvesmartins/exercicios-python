56cad3078031e06ccb313f6c542bc8da8863eafe
Mayara Alves <mayalvesmartins@gmail.com>
2025-07-22 21:18:33 -0300

    exercicios

---
+++ PythonExercicios/Ex0005.py
@@ -0,0 +1,8 @@
+a = input ('digite algo: ')
+print ('o tipo primitivo desse valor e ', type (a))
+print ('so tem espacos? ', a.isspace())
+print ('e um numero? ', a.isnumeric())
+print ('e alfabetico? ', a.isalpha())
+print ('estra em maiusculas? ', a.isupper())
+print ('esta em minusculas ', a.islower())
+print ('esta capitalizada? ', a.istitle())
